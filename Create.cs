f431417423431611e17ead9f9814dc4687ac8435
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-06 18:51:47 +0300

    Create a new gameObject like a prefab

---
+++ Create.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    public GameObject thePrefab;
+    void Start()
+    {
+        GameObject instance = new GameObject();
+        instance = Instantiate(thePrefab, transform.position, transform.rotation) as GameObject;
+    }
+
+    
+}
