eeda0f2039c919f578af517bdaa80d6301f51045
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-06 19:01:10 +0300

    Find any object on the scene and destroys it after 3 seconds

---
+++ FindAndDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindAndDestroy : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(GameObject.Find("Cube"), 3);
+    }
+}
