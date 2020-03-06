7ebeea500ddb2731f565e586f0527789409de714
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-06 19:07:46 +0300

    Just a simple timer

---
+++ Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float myTimer = 5.0f;
+    void Update()
+    {
+        if (myTimer > 0)
+        {
+            myTimer -= Time.deltaTime;
+        }
+        else if (myTimer <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
+}
