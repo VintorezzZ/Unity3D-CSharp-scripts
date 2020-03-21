755f9fe21e8f0cf3b51c714307c55763a2d900fb
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-21 12:48:48 +0300

    Look rotation to the target

---
+++ Aim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    Transform cube;
+    
+    void Update()
+    {
+        Vector3 directionToFace = cube.position - transform.position;
+
+        Debug.DrawRay(transform.position, directionToFace, Color.red);
+
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
