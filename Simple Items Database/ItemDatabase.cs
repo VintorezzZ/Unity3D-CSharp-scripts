691528b01ad8decf0f312b24d66de1a953bb2dcc
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-22 22:12:33 +0300

    Example of the simple items database, which can be edited through inspector in unity and through script itself

---
+++ Simple Items Database/ItemDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public Item[] items;
+
+    public Item pistol;
+    public Item rifle;
+    public Item sword;
+
+    private void Start()
+    {
+        CreateItem();
+    }
+    private void CreateItem()
+    {
+        pistol = new Item("m1911", 1, "This is a legendary classic!");
+        rifle = new Item("Ak-47", 2, "This is a russian kill machine!");
+        sword = new Item("Sword", 3, "Meh, just an iron sword..");
+    }
+}
