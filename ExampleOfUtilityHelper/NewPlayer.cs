5cbfd583bd47fbf7fd6d0ba5211cbce1946b7432
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-23 21:24:37 +0300

    Will comment it later

---
+++ ExampleOfUtilityHelper/NewPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UtilityHelper.CreateObject();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UtilityHelper.SetPositionToZero(this.gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            UtilityHelper.ChangeColor(this.gameObject, Color.red, true);
+        }
+    }
+}
---
+++ ExampleOfUtilityHelper/UtilityHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilityHelper
+{
+    public static void CreateObject()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    public static void SetPositionToZero(GameObject obj)
+    {
+        obj.transform.position = Vector3.zero;
+    }
+
+    public static void ChangeColor(GameObject obj, Color color, bool randomColor = false)
+    {
+        if(randomColor == true)
+        {
+            color = new Color(Random.value, Random.value, Random.value);
+        }
+               
+        obj.GetComponent<MeshRenderer>().material.color = color;       
+        
+    }
+}
