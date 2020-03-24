f25e702dda112e09fb33c1fa0abb258b34f2ec9d
VintorezzZ <sergeevmisha16@gmail.com>
2020-03-24 21:23:45 +0300

    Enum ex

---
+++ Enum_examples/EnemyAI/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public enum EnemyState
+    {
+        Patrolling,
+        Attacking, 
+        Chasing,
+        Death
+    }
+
+    public EnemyState currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.Patrolling;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                Debug.Log("Patrolling...");
+
+                if(Time.time > 5)
+                {
+                    currentState = EnemyState.Chasing;
+                }
+
+                break;
+
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                break;
+
+            case EnemyState.Chasing:
+                Debug.Log("Chasing");
+                break;
+
+            case EnemyState.Death:
+                Debug.Log("Death");
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentState = EnemyState.Attacking;
+        }
+    }
+}
