bddfeddb32d8b7c4ada9359ed784000e78395906
VintorezzZ <sergeevmisha16@gmail.com>
2020-04-02 11:24:15 +0300

    .

---
+++ Delegates and events/Delegate of  type return without parameters/Func.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Func : MonoBehaviour
+{
+    public Func<int> onGetName;
+
+    private void Start()
+    {
+        onGetName = () => //this.gameObject.name.Length;
+        {
+            return this.gameObject.name.Length;
+        };
+        int characterCount = onGetName();
+
+        Debug.Log("Character count: " + characterCount);
+
+    }
+
+    //int GetName()
+    //{
+    //    return this.gameObject.name.Length;
+    //}
+}
