8267d8a22edf761096abb762954941b2b5ccd57f
VintorezzZ <sergeevmisha16@gmail.com>
2020-04-03 08:56:53 +0300

    d

---
+++ Delegates and events/Delegate of type void with parameters/Action.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Lambda : MonoBehaviour
+{
+    public Action<int, int> Sum;
+    void Start()
+    {
+        Sum = (a, b) =>
+        {
+            int total = a + b;
+            if (total < 100)
+            {
+                Debug.Log("total is less of 100");
+            }
+
+            Debug.Log("Total: " + total);
+        };
+
+        Sum(5, 4);
+    }
+
+   
+}
