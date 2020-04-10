3d50460583bf191d51e40cff75961cb8e578bcf4
VintorezzZ <sergeevmisha16@gmail.com>
2020-04-10 14:43:05 +0300

    .

---
+++ Delegates and events/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public void Start()
+    {
+        Main1.onClick += Spawn;
+    }
+    public void Spawn()
+    {
+        transform.position = new Vector3(4, 2, -4);
+    }
+
+    private void OnDisable()
+    {
+        Main1.onClick -= Spawn;
+    }
+}
