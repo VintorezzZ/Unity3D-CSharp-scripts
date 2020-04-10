abbff92f848c07df738809b7c48b4fcdcd6de047
VintorezzZ <sergeevmisha16@gmail.com>
2020-04-10 14:41:55 +0300

    Example of simple Event system

---
+++ Delegates and events/Main1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main1 : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+
+    public void ButtonClick()
+    {
+        if(onClick != null)
+        {
+            onClick();
+        }
+    }
+
+}
