8d727ec40fa4235e5af1b521090d44424409faef
VintorezzZ <sergeevmisha16@gmail.com>
2020-04-10 14:42:24 +0300

    .

---
+++ Delegates and events/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main1.onClick += TurnRed;
+    }
+
+    public void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnDisable()
+    {
+        Main1.onClick -= TurnRed;
+    }
+}
